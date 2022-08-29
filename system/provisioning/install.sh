125ba3c3d13d1d52492dc01cfaae08cb30b70d56
x <x@fopsfire.net>
2022-08-29 15:28:16 -0700

    Added basic function test

--- system/provisioning/install.sh
+++ system/provisioning/install.sh
@@ -8,14 +8,18 @@ sudo DEBIAN_FRONTEND=noninteractive apt-get update -y && \
     libespeak1 \
     portaudio19-dev
 
+cp -r ./ /tmp/radiodtmf/
+
 pushd /tmp/radiodtmf || exit
 sudo python -m pip install .
 popd || exit
 
+# Sanity check to see if program sort of works
+/usr/local/bin/python -m radiodtmf --version
+
 sudo install -o root -g root -m 600 \
     /tmp/radiodtmf/system/default/radiodtmf \
     /etc/default/radiodtmf
-
 
 sudo install -o root -g root -m 644 \
     /tmp/radiodtmf/system/systemd/radiodtmf.service \
